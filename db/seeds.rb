b59eaa6afde75925fcb6e8a388b9ecae162bbfee
zSteinO34 <zstein1234@gmail.com>
2019-09-24 09:47:20 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+pops = Artist.create(name: "Big Poppa OOF", bio: "Big Poppa OOF is a 30-year-old Musician who enjoys competitive dog grooming, playing card games and working on cars. 
+    He is entertaining and energetic, but can also be very greedy and a bit unintelligent.
+    He is a Swedish Buddhist that didn't finish school.
+    Physically, Big is not in great shape. He needs to lose quite a lot of weight. He is short with olive skin, black hair and brown eyes.
+    He grew up in an upper class neighbourhood. He was raised by his mother, his father having left when he was young.
+    He is currently single. His most recent romance was with a doctor called Gabriella Maja Richards, who was 8 years older than him. They broke up because Big wanted somebody more focussed on having fun.
+    Big's best friend is a tradesperson called Elisabeth Gonzales. They have a very firey friendship. He also hangs around with Alexandra Chen and Lilly Adam. They enjoy going to the movies together.")
